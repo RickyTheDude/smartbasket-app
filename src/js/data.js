762c21e9e3d29c38f011c462579acda3c6f93ba8
Ayan Mahata <91912251+RickyTheDude@users.noreply.github.com>
2025-07-14 12:11:13 +0400

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -1,7 +1,7 @@
 // src/js/data.js
 
 export const recommendations = {
-    'ketchup': { name: 'Heinz Tomato Ketchup', description: 'The classic, highly-rated favorite.', image: 'https://placehold.co/100x100/e53e3e/ffffff?text=Ketchup', shelf: 4, row: 2 },
+    'ketchup': { name: 'American Garden Tomato Ketchup', description: 'The classic, highly-rated favorite.', image: 'https://ibb.co/FkdQvbVM', shelf: 4, row: 2 },
     'milk': { name: 'Organic Valley Whole Milk', description: 'Fresh, organic, and full of flavor.', image: 'https://placehold.co/100x100/4299e1/ffffff?text=Milk', shelf: 12, row: 1 },
     'bread': { name: 'Dave\'s Killer Bread', description: 'Packed with protein, fiber, and whole grains.', image: 'https://placehold.co/100x100/975a16/ffffff?text=Bread', shelf: 2, row: 3 }
 };
